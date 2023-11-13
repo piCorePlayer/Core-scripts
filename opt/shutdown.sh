e91302ca9ecaf2a5ac90a2585fd0c959b156393d
paul-1 <6473457+paul-1@users.noreply.github.com>
2023-11-13 18:57:18 -0500

    Update mirror for pi5 second kernel and add lms shutdown to default shutdown

--- opt/shutdown.sh
+++ opt/shutdown.sh
@@ -10,6 +10,13 @@ case $ACTION in
 	*);;
 esac
 
+if [ -x /usr/local/etc/init.d/slimserver ]; then
+	PID=$(pidof slimserver.pl)
+	if [ -n "$PID" ]; then
+		/usr/local/etc/init.d/slimserver stop
+	fi
+fi
+
 # If no backup of home was done then loop through valid users to clean up.
 if [ ! -e /tmp/backup_done ] || ! grep -q "^home" /opt/.filetool.lst; then
   awk 'BEGIN { FS=":" }  $3 >= 1000 && $1 != "nobody" { print $1 }' /etc/passwd > /tmp/users
@@ -23,6 +30,6 @@ if [ ! -e /tmp/backup_done ] || ! grep -q "^home" /opt/.filetool.lst; then
           rm -f "$TARGET"
         fi
       fi
-    done < /opt/.xfiletool.lst      
-  done < /tmp/users 
+    done < /opt/.xfiletool.lst
+  done < /tmp/users
 fi
